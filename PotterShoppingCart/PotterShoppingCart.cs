3cdbfa9ea2e618baa82b35f3c943ccb117893701
jill90698 <jill90698@hotmail.com>
2015-09-23 16:56:07 +0800

    Modify implement logic, let first and second test will pass.

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,63 @@ namespace PotterShoppingCart
 
         public decimal ComputeOrderAmount(IDictionary<PotterEpisode, int> order)
         {
-            decimal sum = order.Sum(o => o.Value * UnitPrice);
+            decimal sum = 0;
+            decimal discount;
+
+            //Convert to Order model
+            var newOrder = order.Select(x => new Order() {PotterEpisode = x.Key, Count = x.Value}).ToList();
+
+            //Business logic
+            while (newOrder.Any())
+            {
+                //Each loop will sum one times of a group of distinct potterEpisode book.
+                int distinctBookCount = newOrder.Count();
+
+                switch (distinctBookCount)
+                {
+                    case 2:
+                        discount = 0.95m;                        
+                        break;
+                    default:
+                        discount = 1;
+                        break;
+                }
+
+                sum += newOrder.Sum(o => UnitPrice * discount);
+
+                //After compute finish, remove this group of distinct potterEpisode book.
+                RemoveComputedBooks(newOrder);
+            }
+ 
             return sum;
         }
+
+        private void RemoveComputedBooks(IList<Order> order)
+        {
+            //Deducted the count of each order
+            List<Order> wantedRemoveList = new List<Order>();
+            foreach (var item in order)
+            {
+                item.Count--;
+                if (item.Count == 0)
+                {
+                    wantedRemoveList.Add(item);
+                }
+            }
+
+            //Remove the count equal zero item in order list
+            foreach (var item in wantedRemoveList)
+            {
+                order.Remove(item);
+            }
+        }
+    }
+
+    public class Order
+    {
+        public PotterEpisode PotterEpisode { get; set; }
+
+        public int Count { get; set; }
     }
 
     public enum PotterEpisode
@@ -24,5 +79,6 @@ namespace PotterShoppingCart
         Fourth,
         Fifth
     }
+
   
 }
