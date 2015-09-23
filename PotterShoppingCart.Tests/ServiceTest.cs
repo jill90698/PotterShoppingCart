32ba18900f163a171e790227311f01a35498656e
jill90698 <jill90698@hotmail.com>
2015-09-23 14:26:35 +0800

    first version

---
+++ PotterShoppingCart.Tests/ServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    /// <summary>
+    /// Summary description for ServiceTest
+    /// </summary>
+    [TestClass]
+    public class ServiceTest
+    {
+        public ServiceTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Test_PotterShoppingCart_Only_1_first_book_should_be_100()
+        {
+            //Arrange
+            var target = new PotterShoppingCart();
+
+            var order = new Dictionary<PotterEpisode, int>();
+            order.Add(PotterEpisode.First, 1);
+
+            decimal expected = 100;
+
+            //Act
+            decimal actual = target.ComputeOrderAmount(order);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        private decimal UnitPrice = 100;
+
+        public decimal ComputeOrderAmount(IDictionary<PotterEpisode, int> order)
+        {
+            decimal sum = order.Sum(o => o.Value * UnitPrice);
+            return sum;
+        }
+    }
+
+    public enum PotterEpisode
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+  
+}
